ee62a03048b6f2501d4d72a438be3bb15c8eebd6
Harshit Daftary <daftaryharshit@yahoo.co.in>
2021-01-19 23:27:18 +0530

    Swift package (#1)
    * Added package.swift to support swift package
    * added file name
    * updated by adding ios version

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AlamofireURLCache5",
+    platforms: [
+        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "AlamofireURLCache5",
+            targets: ["AlamofireURLCache5"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "AlamofireURLCache5",
+            dependencies: [],
+            path: "./AlamofireURLCache",
+            sources: ["AlamofireURLCache.swift"]),
+        .testTarget(
+            name: "AlamofireURLCache5Tests",
+            dependencies: ["AlamofireURLCache5"]),
+    ]
+)
